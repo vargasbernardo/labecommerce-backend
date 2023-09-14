d90e4942c5ddb4201cbb1abbe32370617459246e
Bernardo <vargasbernardo97@gmail.com>
2023-09-14 20:13:50 -0300

    feat: adicionados endpoints PUT para edição de produtos e DELETE para deletar produtos pelo id e usuarios pelo id. documento formatado

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,14 @@
 export type TUser = {
-    id: string,
-    name: string,
-    email: string,
-    password: string,
-    createdAt: string,
-
-}
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  createdAt: string;
+};
 export type TProduct = {
-    id: string,
-    name: string,
-    price: number,
-    description: string,
-    imageUrl: string,
-}
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  imageUrl: string;
+};
