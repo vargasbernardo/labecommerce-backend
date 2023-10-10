306c9c321d06bd4e8b7166868ea1f3abe4244fd4
Bernardo <vargasbernardo97@gmail.com>
2023-10-10 13:15:57 -0300

    tabela de compras criada e relacionada

--- src/database/labecommerce.sql
+++ src/database/labecommerce.sql
@@ -8,12 +8,13 @@ CREATE TABLE
         name TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL,
-        created_at TEXT DEFAULT(DATETIME())NOT NULL
+        created_at TEXT DEFAULT(DATETIME()) NOT NULL
     );
 
 -- Populando a tabela de usuarios
 
-INSERT INTO users (id, name, email, password)
+INSERT INTO
+    users (id, name, email, password)
 VALUES (
         'u001',
         'Fulano',
@@ -36,6 +37,7 @@ VALUES (
 SELECT * from users 
 
 -- Deletar tabela de users
+
 DROP TABLE users;
 
 -- Criacao da tabela de produtos
@@ -85,28 +87,113 @@ VALUES (
     );
 
 --pegar a tabela de produtos
+
 SELECT * FROM products;
 
 -- criar um novo user
-INSERT INTO users (id, name, email, password)
-VALUES ('p0043', 'Jon Jones', 'jon.jones@gmail.com', '00933445f');
+
+INSERT INTO
+    users (id, name, email, password)
+VALUES (
+        'u004',
+        'Jon Jones',
+        'jon.jones@gmail.com',
+        '00933445f'
+    );
 
 -- criar um novo produto
+
 INSERT INTO products
-VALUES ('p0057', 'tests', 'teste', 'teste', 'teste');
+VALUES (
+        'p006',
+        'tests',
+        'teste',
+        'teste',
+        'teste'
+    );
 
 -- Retornar produtos especificos
+
 SELECT * FROM products WHERE name LIKE '%gamer%';
 
-
 -- deletar user por ID
+
 DELETE FROM users WHERE id = 'u001';
 
-
 --deletar produto por ID
+
 DELETE FROM products WHERE id = 'p001';
 
 -- editar produtos por ID
+
 UPDATE products
-SET name = 'teste', price = 40.99, description = 'teste', image_url = 'teste'
+SET
+    name = 'teste',
+    price = 40.99,
+    description = 'teste',
+    image_url = 'teste'
 WHERE id = 'p001';
+
+-- Criacao da tabela de pedidos
+
+CREATE TABLE
+    purchases (
+        id TEXT PRIMARY KEY UNIQUE NOT NULL,
+        buyer TEXT NOT NULL,
+        total_price REAL NOT NULL,
+        created_at TEXT DEFAULT(DATETIME()) NOT NULL,
+        FOREIGN KEY (buyer) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+    );
+
+DROP TABLE purchases;
+
+-- popular tabela de pedidos
+
+INSERT INTO
+    purchases (id, buyer, total_price)
+VALUES ('pu001', 'u001', 44.5), ('pu002', 'u003', 32), ('pu003', 'u002', 100.99), ('pu004', 'u002', 10000);
+
+SELECT * FROM purchases 
+
+--editar pedido
+
+UPDATE purchases SET total_price = 300 WHERE id = 'pu001';
+
+-- consulta com juncao(endpoint de informacoes de uma compra especifica)
+
+SELECT
+    purchases.id,
+    users.id,
+    users.name,
+    users.email,
+    purchases.total_price,
+    purchases.created_at
+FROM purchases
+    INNER JOIN users ON purchases.buyer = users.id;
+
+--criacao tabela de relacoes
+CREATE TABLE purchases_products (
+    purchase_id TEXT NOT NULL,
+    product_id TEXT NOT NULL,
+    quantity INTEGER NOT NULL,
+    FOREIGN KEY (purchase_id) REFERENCES purchases (id),
+    FOREIGN KEY (product_id) REFERENCES products (id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+DROP TABLE purchases_products;
+
+-- populando tabela purchases_products
+INSERT INTO purchases_products
+VALUES 
+    ('pu001', 'p005', 1),
+    ('pu002', 'p002', 1),
+    ('pu003', 'p003', 2);
+
+-- consulta com INNER JOIN
+SELECT * FROM purchases_products AS pp
+INNER JOIN products AS pr ON pp.product_id = pr.id
+INNER JOIN purchases AS pu ON pp.purchase_id = pu.id;
